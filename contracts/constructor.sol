3bd6f92d498d9bcd060a921af27bb75c38cbee7e
itsMuli <rosemwendwa163@gmail.com>
2024-05-30 13:00:35 +0300

    sender changes

--- contracts/constructor.sol
+++ contracts/constructor.sol
@@ -1,3 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
+contract ExampleContract {
+    string public name;
+
+    // if you use calldata, it won't compile
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
